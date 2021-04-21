b26bec751e17e4297d99514b0209dfc9fafeca68
Ahmed <leapollo2005@gmail.com>
2021-04-21 17:22:58 +0300

    Fixed loop again

--- guessing.v
+++ guessing.v
@@ -1,15 +1,14 @@
 import os
 import rand
 
-const total_tries = 5
+const total_tries = 10
 
 fn main() {
 	mut input_num := 0
 	mut counter := 1
 	println("Hello, Let's guess some numbers!")
-	min := os.input('enter min: ').int()
 	max := os.input('enter max: ').int()
-	secret_number := rand.i64_in_range(min, max + 1)
+	secret_number := rand.intn(max + 1)
 
 	// game loop
 	for input_num != secret_number && counter <= total_tries {
@@ -17,14 +16,15 @@ fn main() {
 		if input_num < secret_number {
 			println('too low')
 			println('tries: $counter')
+			counter++
 		} else if input_num > secret_number {
 			println('too high')
 			println('tries: $counter')
+			counter++
 		} else {
 			println('Congratulations You Won!!')
 			println('took you: $counter tries')
 		}
-		counter++
 	}
 	println('Game Over.')
 	exit(0) // cuze why not
